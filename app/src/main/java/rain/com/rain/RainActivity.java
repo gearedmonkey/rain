155471254361a148e6bd59f2e4a73f7b9dddc587
Jordan <gearedmonkey@gmail.com>
2016-11-13 19:43:38 -0600

    Add text to speech functionality

--- app/src/main/java/rain/com/rain/RainActivity.java
+++ app/src/main/java/rain/com/rain/RainActivity.java
@@ -7,22 +7,29 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 public class RainActivity extends AppCompatActivity {
     public static final String SNOW_BACKGROUND = "SNOW";
     TextView tvTempDescription, tvTemperature, tvWindspeed;
+    Button btnSpeech;
     private final String TAG = "MAIN";
     private Controller t;
+    TextToSpeech t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,32 @@ public class RainActivity extends AppCompatActivity {
         tvTempDescription = (TextView) findViewById(R.id.tvTempDescription);
         tvTemperature = (TextView) findViewById(R.id.tvTemperature);
         tvWindspeed = (TextView) findViewById(R.id.tvWindspeed);
+        btnSpeech = (Button) findViewById(R.id.btnTextSpeech);
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+        btnSpeech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = tvTempDescription.getText().toString();
+                Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, "RainAcitivty");
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        if(t1 !=null){
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
     }
 
     private void initializeLocationListener() {
